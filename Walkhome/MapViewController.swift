9b84fbe140b968e222f866e112d8713e711884c7
Raymond Chung <raymond_chung2@hotmail.com>
2016-07-16 11:41:56 -0400

    Added the mapview with current location

---
+++ Walkhome/MapViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MapViewController.swift
+//  Walkhome
+//
+//  Created by Raymond Chung on 2016-07-16.
+//  Copyright © 2016 COMPSA Web Services. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager() //This will give us the user's current location
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Map"
+        self.locationManager.delegate = self
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Walkhome/Walkhome/ViewController.swift
+++ Walkhome/Walkhome/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mapView: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
